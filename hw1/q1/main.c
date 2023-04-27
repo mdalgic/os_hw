ca4eba9eae827c5606ece576ad74998d4f68f1e0
mdalgic <meric.dalgic@pm.me>
2023-04-27 16:57:09 +0300

    write_bytes functionality was added

--- hw1/q1/main.c
+++ hw1/q1/main.c
@@ -5,8 +5,40 @@
 #define DEFAULT_LINE_AMOUNT 10
 #define VERSION 1.0
 
+int write_bytes(const char* filename, const int byte_amount)
+{
+    // Open the file in binary mode for reading
+    FILE* file = fopen(filename, "rb");
+    if (file == NULL) {
+        fprintf(stderr, "Error opening file: %s\n", filename);
+        return;
+    }
+
+    // Set the file position indicator to the beginning of the file
+    fseek(file, 0L, SEEK_SET);
+
+    // Read and print the requested number of bytes from the file
+    unsigned char buffer[byte_amount];
+
+    size_t bytes_read = fread(buffer, sizeof(unsigned char), byte_amount, file);
+
+    fwrite(buffer, sizeof(unsigned char), bytes_read, stdout);
+
+    // Close the file
+    fclose(file);
+
+    putchar('\n');
+
+    return 0;
+}
 
 
+
+int write_lines(const char* filename, const int line_amount)
+{
+
+    return 0;
+}
 
 int main(int argc, char* argv[])
 {
@@ -15,6 +47,7 @@ int main(int argc, char* argv[])
     int result;
     int flag_bytes, flag_lines, flag_verbose, flag_help, flag_version, flag_error;
     char *arg_bytes, *arg_lines;
+    int line_amount, byte_amount;
 
     struct option options[] = {
         {"bytes",   required_argument,  NULL, 'c'}, 
@@ -93,37 +126,71 @@ int main(int argc, char* argv[])
     // if neither is defined
     if ((flag_bytes | flag_lines) == 0)
     {   
-        // fprintf(stdout, "Neither was defined\n");
+        // optind is the index of the starting point of arguments without option
+        if (optind != argc)
+        {
+            for (int i = optind; i < argc; ++i)
+            {
+                if(flag_verbose)
+                    fprintf(stdout, "[File: %s]\n", argv[i]);
+                write_lines(argv[i], DEFAULT_LINE_AMOUNT);
+            }
+        }
 
-        flag_lines = 1;
-        // arg_lines = DEFAULT_LINE_AMOUNT;
+        // TODO: Add this part.
+        // There is no input file
+        else
+        {
+
+        }
     }
 
 
     if(flag_bytes)
     {
+        byte_amount = strtol(arg_bytes, NULL, 10);
 
+        if(byte_amount == 0)
+            fprintf(stderr, "Byte amount:%s could not be parsed correctly!\n", arg_bytes);
+        else
+        {
+            // optind is the index of the starting point of arguments without option
+            if (optind != argc)
+            {
+                for (int i = optind; i < argc; ++i)
+                {
+                    if(flag_verbose)
+                        fprintf(stdout, "[File: %s]\n", argv[i]);
+                    write_bytes(argv[i], byte_amount);
+                }
+            }
+        }  
     }
 
     if(flag_lines)
     {
+        line_amount = strtol(arg_lines, NULL, 10);
 
-    }
-
-    if(flag_verbose)
-    {
-        // optind is the index of the starting point of arguments without option
-        if (optind != argc)
+        if(line_amount == 0)
+            fprintf(stderr, "Line amount:%s could not be parsed correctly!\n", arg_lines);
+        else
         {
-            fprintf(stdout, "-------Files--------\n");
-
-            for (int i = optind; i < argc; ++i)
-                puts(argv[i]);
-            
-            fprintf(stdout, "--------------------\n");
+            // optind is the index of the starting point of arguments without option
+            if (optind != argc)
+            {
+                for (int i = optind; i < argc; ++i)
+                {
+                    if(flag_verbose)
+                        fprintf(stdout, "[File: %s]\n", argv[i]);
+                    write_lines(argv[i], line_amount);
+                }
+            }
         }
-    }
+        
 
+        
+
+    }
 
     if(flag_help)
     {
