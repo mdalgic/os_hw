0f1b0402c32cbf486ddd4779e87e2197bb249ea5
mdalgic <meric.dalgic@pm.me>
2023-04-27 22:38:42 +0300

    Parsing options and arguments was completed

--- hw2/q1/main.c
+++ hw2/q1/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 
 
 /*
@@ -26,7 +28,20 @@ possible conversion types,
 multiple conversion types can exist with comma delimiter
 
 dd command replaces the destination file by default, it overrides. To prevent it use excl.
+
+note: ucase and lcase are mutually exclusive. If both of them exist, error occurs.
 */
+
+
+/// @brief This function will be used to check the correctness of given filepaths
+void check_filepath(const char* filepath)
+{
+    if(access(filepath, F_OK) == -1)
+    {
+        fprintf(stderr, "Filepath is not valid: %s\n", filepath);
+        exit(EXIT_FAILURE);
+    }
+}
 
 
 int main(int argc, char* argv[])
@@ -38,6 +53,13 @@ int main(int argc, char* argv[])
 
     char arg_if[1024], arg_of[1024], arg_bs[20], arg_count[20], arg_conv[200];
 
+    int block_size, block_count;
+    block_size = block_count = 0;
+
+    int flag_conv_ucase, flag_conv_lcase, flag_conv_excl;
+    flag_conv_ucase = flag_conv_lcase = flag_conv_excl = 0;
+
+    // Pulling options and arguments from command line
     for(int i=1; i < argc; ++i)
     {
         if(strncmp(argv[i], "if=", 3) == 0)
@@ -84,8 +106,8 @@ int main(int argc, char* argv[])
                 exit(EXIT_FAILURE);
             }
 
-            flag_bs = 1;
-            strcpy(arg_bs, &argv[i][6]);
+            flag_count = 1;
+            strcpy(arg_count, &argv[i][6]);
         }
 
         else if(strncmp(argv[i], "conv=", 5) == 0)
@@ -112,7 +134,84 @@ int main(int argc, char* argv[])
         }
     }
 
-    
+    // Checking and parsing arguments
+    if(flag_if)
+        check_filepath(arg_if);
+
+    if(flag_of)
+        check_filepath(arg_of);
+
+    if(flag_bs)
+    {
+        char *endptr;
+        block_size = strtol(arg_bs, &endptr, 10);
+
+        if(block_size <= 0 || *endptr != '\0')
+        {
+            fprintf(stderr, "Invalid block size %s\n", arg_bs);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if(flag_count)
+    {
+        char *endptr;
+        block_count = strtol(arg_count, &endptr, 10);
+
+        if(block_count <= 0 || *endptr != '\0')
+        {
+            fprintf(stderr, "Invalid block count %s\n", arg_count);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if(flag_conv)
+    {
+        char *token = strtok(arg_conv, ",");
+        while(token != NULL)
+        {
+            if(strcmp(token, "ucase") == 0)
+            {
+                flag_conv_ucase = 1;
+            }
+
+            else if(strcmp(token, "lcase") == 0)
+            {
+                flag_conv_lcase = 1;
+            }
+
+            else if(strcmp(token, "excl") == 0)
+            {
+                flag_conv_excl = 1;
+            }
+
+            else
+            {
+                fprintf(stderr, "Undefined conversion type: %s\n", token);
+                exit(EXIT_FAILURE);
+            }
+
+            token = strtok(NULL, ",");
+        }
+    }
+
+    // note: ucase and lcase are mutually exclusive. If both of them exist, error occurs.
+    if(flag_conv_ucase && flag_conv_lcase)
+    {
+        fprintf(stderr, "ucase and lcase cannot be used together\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Checking if of and if are same
+    if(flag_of && flag_if)
+    {
+        if(strcmp(arg_of, arg_if) == 0)
+        {
+            fprintf(stderr, "of and if cannot be same\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
     puts("\n");
  
     return 0;
